f26f00e134e588a8df4f66aa1fcd10ea24096145
naufal-invictus <143852075+naufal-invictus@users.noreply.github.com>
2025-05-05 14:17:48 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Facebook Cover Generaot',
+  description: 'Generaot',
 }
 
 export default function RootLayout({
